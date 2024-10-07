cdb9a584d0915fac34c72c6a012b979adfa9ecd3
Juha Loukaja <loukaja@gmail.com>
2024-10-07 09:53:12 +0300

    Add word value calculation and optional displaying of optimal word

--- game.py
+++ game.py
@@ -33,13 +33,53 @@ def _validation(word, draw):
 
     return any(word.upper() == dict_word.upper() for dict_word in DICTIONARY)
 
+def calculate_word_value(word):
+    return(sum(LETTER_SCORES.get(char.upper(), 0) for char in word))
+
+def get_possible_dict_words(draw):
+    """Get all possible words from draw which are valid dictionary words.
+    Use the _get_permutations_draw helper and DICTIONARY constant"""
+
+    dictionary_set = {word.upper() for word in DICTIONARY}
+    perm_set = _get_permutations_draw(draw)
+    possible_words = list(perm_set & dictionary_set)
+
+    return possible_words
+
+
+def _get_permutations_draw(draw):
+    """Helper for get_possible_dict_words to get all permutations of draw letters.
+    Hint: use itertools.permutations"""
+    permutations = set(
+        ''.join(perm).upper()  # Join tuples into strings and convert to uppercase
+        for length in range(1, 8)  # Generate permutations for lengths 1 to 7
+        for perm in itertools.permutations(draw, length)
+    )
+
+    return permutations
+
+def max_word_value(words):
+    """Calc the max value of a collection of words"""
+    return max(words, key=calculate_word_value)
 
 def main():
     draw = draw_letters()
     print("Nostetut kirjaimet: {}".format(', '.join(draw)))
 
     word = input_word(draw)
-    print(f"Syötit sanan: {word}")
+    word_value = calculate_word_value(word)
+    print(f"Syötit sanan: {word}, sen arvo on: {word_value}")
+
+    print_max_word = input("Haluatko nähdä optimaalisen sanan (k/e)?")
+    if print_max_word.lower() == 'k':
+        possible_words = get_possible_dict_words(draw)
+
+        max_word = max_word_value(possible_words)
+        max_word_score = calculate_word_value(max_word)
+        if max_word_score == word_value:
+            print("Onnistuit jo löytämään optimaalisen sanan, onnittelut!")
+        else:
+            print(f"Optimaalinen sana olisi ollut {max_word}, jonka pistearvo on {max_word_score}.")
 
 
 if __name__ == "__main__":
